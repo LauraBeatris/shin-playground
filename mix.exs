83bd18f39c490c0f85596e70ef602ecef2f3dd8d
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-03-25 09:57:18 -0300

    Deploy with Fly.io

--- mix.exs
+++ mix.exs
@@ -52,7 +52,8 @@ defmodule ShinPlayground.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:shin_auth, "~> 1.0.0"}
     ]
   end
 
