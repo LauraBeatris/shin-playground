24a5b481cd877c0263550855d91ad02c48399b23
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-03-26 20:24:54 -0300

    Render draft for sidebar

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -22,6 +22,12 @@ module.exports = {
       },
       backgroundImage: {
         'gradient': 'linear-gradient(90deg, rgb(255, 255, 255) 0%, rgb(242, 237, 237) 100%)'
+      },
+      borderRadius: {
+        navbar: '20px'
+      },
+      colors: {
+        'navbar-icon': 'rgba(255, 255, 255, 0.1)'
       }
     },
   },
