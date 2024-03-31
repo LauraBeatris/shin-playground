7c9c8f0b2d7528b75023dabb381a87ee4ae1e832
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-03-31 11:54:20 -0300

    Trigger loader with JS commands

--- lib/shin_playground_web/live/oidc/oidc_action_component.ex
+++ lib/shin_playground_web/live/oidc/oidc_action_component.ex
@@ -9,7 +9,10 @@ defmodule ShinPlaygroundWeb.OIDC.OIDCActionComponent do
 
     socket =
       socket
-      |> assign(oidc_loading: true)
+      |> push_event("js-exec", %{
+        to: "#oidc_loader",
+        attr: "data-wait"
+      })
 
     {:noreply, socket}
   end
@@ -21,10 +24,57 @@ defmodule ShinPlaygroundWeb.OIDC.OIDCActionComponent do
     socket =
       socket |> assign(decoded_oidc_provider_configuration: decoded_oidc_provider_configuration)
 
+    socket =
+      socket
+      |> push_event("js-exec", %{
+        to: "#oidc_loader",
+        attr: "data-done"
+      })
+
     {:ok, socket}
   end
 
   def update(regular_assigns, socket) do
     {:ok, assign(socket, regular_assigns)}
+  end
+
+  def loader(assigns) do
+    ~H"""
+    <svg
+      class="hidden animate-spin h-5 w-5 text-gray-800"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      id={@id}
+      data-wait={show_loader()}
+      data-done={hide_loader()}
+    >
+      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4">
+      </circle>
+      <path
+        class="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      >
+      </path>
+    </svg>
+    """
+  end
+
+  defp show_loader() do
+    JS.hide(%JS{},
+      to: "#oidc_empty_state"
+    )
+    |> JS.show(
+      to: "#oidc_loader",
+      transition: {"ease-out duration-300", "opacity-0", "opacity-100"}
+    )
+  end
+
+  defp hide_loader() do
+    JS.hide(%JS{},
+      to: "#oidc_loader",
+      transition: {"ease-in duration-300", "opacity-100", "opacity-0"}
+    )
   end
 end
